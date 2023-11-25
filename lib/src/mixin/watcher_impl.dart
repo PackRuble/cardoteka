0b5238b0454505c62b7edcf733600c7d7509ccff
Ruble <pack.ruble@gmail.com>
2023-11-25 16:27:01 +0300

    improve: WatcherImpl.attach has become semantically more expressive
    * eliminated unnecessary check for null
    * documentation updated

--- lib/src/mixin/watcher_impl.dart
+++ lib/src/mixin/watcher_impl.dart
@@ -6,7 +6,8 @@ import '../watcher.dart';
 
 // ignore_for_file: prefer_function_declarations_over_variables
 
-typedef CbWatcher<V> = void Function(V value);
+
+typedef ValueCallback<V extends Object?> = void Function(V value);
 
 typedef Detacher = void Function(void Function());
 
@@ -15,57 +16,65 @@ mixin WatcherImpl on Cardoteka implements Watcher {
   @override
   Watcher get watcher => this;
 
-  final _watchers = <Card, List<CbWatcher>>{};
+  final _watchers = <Card, List<ValueCallback>>{};
 
   @override
   @internal
   void notify<V extends Object>(Card<V?> card, V? value) {
-    final ws = _watchers[card];
+    final List<ValueCallback<V?>>? callbacksByCard = _watchers[card];
 
-    if (ws != null) {
-      for (final watcher in ws) {
-        watcher.call(value);
+    if (callbacksByCard != null) {
+      for (final cb in callbacksByCard) {
+        cb.call(value);
       }
     }
   }
 
-  /// Attach a [CbWatcher] to your [Card]. A [watcher] will be called whenever
-  /// the [card] value changes.
+  /// Attach a [ValueCallback] to your [Card]. A [callback] will be called
+  /// whenever the [card] value changes.
   ///
   /// Use this method when you want to track changes in the value for [card].
-  /// As soon as you call [Cardoteka.set] the value is passed to the listener [watcher].
-  /// If your listener can be deleted, pass [detacher], thereby freeing up related resources.
+  /// As soon as you call:
+  /// - [Cardoteka.set] or [Cardoteka.setOrNull]
+  /// - [Cardoteka.remove] or [Cardoteka.removeAll]
+  /// the value is passed to the listener [callback].
   ///
-  /// The first call returns the stored value from the database. If null, returned
-  /// default value [Card.defaultValue] for the given [card].
+  /// If your listener can be deleted, pass [detacher], thereby freeing up
+  /// related resources. The necessity of this argument is due to the high degree
+  /// of forgetfulness of its instruction.
+  ///
+  /// The call will return the stored value from storage. If there was no value,
+  /// [Card.defaultValue] will be returned.
+  ///
   V attach<V extends Object?>(
     Card<V> card,
-    CbWatcher<V> watcher, {
-    // The obligativeness of the argument [detacher] is due to the high degree
-    //  of forgetfulness of its instruction
+    ValueCallback<V> callback, {
     required Detacher? detacher,
   }) {
-    final w = (Object? value) => watcher(value as V);
-
-    _watchers[card] = [...?_watchers[card], w];
+    // we create a new callback based on an existing one because
+    // type 'void Function(V)' can't be assigned
+    //   to 'void Function(Object?)'
+    final newCallback = (Object? value) => callback(value as V);
+    final callbacksByCard =
+        _watchers.putIfAbsent(card, () => <ValueCallback>[]);
+    callbacksByCard.add(newCallback);
 
     detacher?.call(() {
-      _watchers[card]?.remove(w);
-
-      // Remove the key from the [_watchers], if the list is empty
-      if (_watchers[card]?.isEmpty ?? false) {
+      callbacksByCard.remove(newCallback);
+      if (callbacksByCard.isEmpty) {
         _watchers.remove(card);
       }
     });
 
-    return getOrNull(card) ?? card.defaultValue;
+    final V value = getOrNull(card) ?? card.defaultValue;
+    return value;
   }
 }
 
 @visibleForTesting
 mixin WatcherImplDebug on WatcherImpl {
   @visibleForTesting
-  Map<Card<Object?>, List<CbWatcher>> get watchersDebug => _watchers;
+  Map<Card, List<ValueCallback>> get watchersDebug => _watchers;
 
   @visibleForTesting
   void printAllWatchers() {
