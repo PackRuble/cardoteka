a2d561836ea4980ed44785dfda99374e05fbc233
Ruble <pack.ruble@gmail.com>
2023-11-29 13:23:40 +0300

    add: Ability to test several sets of cards
    - attach: return when storage contains value

--- test/watcher_impl_test.dart
+++ test/watcher_impl_test.dart
@@ -1,8 +1,15 @@
+// @dart = 3.2
+
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:cardoteka/cardoteka.dart';
 import 'package:cardoteka/src/core.dart' show CardotekaUtilsForTest;
+import 'package:cardoteka/src/extensions/data_type_ext.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-enum FishCard<T> implements Card<T> {
+import 'source/forest_key_store.dart';
+
+enum FishCard<T extends Object?> implements Card<T> {
   perch<int>(DataType.int, 2023),
   perchGhost<int?>(DataType.int, 2024),
   perchGhostNull<int?>(DataType.int, null),
@@ -20,6 +27,42 @@ enum FishCard<T> implements Card<T> {
   String get key => name;
 }
 
+final cardsCollections = [
+  CardConfig(
+    name: '$FishCard',
+    cards: FishCard.values,
+  ),
+  CardConfig(
+    name: '$ForestCard',
+    cards: ForestCard.values,
+    converters: ForestCard.converters,
+  ),
+];
+
+bool isPrimitiveValue(Card card, {required bool elseNull}) {
+  final Object? defaultValue = card.defaultValue;
+  if (defaultValue == null) return elseNull;
+
+  return card.type.isCorrectType(defaultValue);
+}
+
+Object? getTestValueBasedOnDefaultValue(Card<Object?> card) {
+  final Object? defaultValue = card.defaultValue;
+  if (defaultValue == null) return null;
+  return switch (card.type) {
+    DataType.string => (defaultValue as String) + '_test',
+    DataType.int => (defaultValue as int) * 2,
+    DataType.double => (defaultValue as double) + 0.1,
+    DataType.bool => !(defaultValue as bool),
+    DataType.stringList => (defaultValue as List<String>) + ['_test'],
+  };
+}
+
+String getReason(String message, Card card) => '''
+$message
+Broken on cards: $card
+''';
+
 class CardotekaTest extends Cardoteka
     with WatcherImpl, WatcherImplDebug, CardotekaUtilsForTest, AccessToSP {
   CardotekaTest({required super.config});
@@ -27,82 +70,142 @@ class CardotekaTest extends Cardoteka
 
 typedef AsyncCallback = Future<void> Function();
 
-void main() {
-  late CardotekaTest cardoteka;
-  setUp(() async {
-    cardoteka = CardotekaTest(
-      config: const CardConfig(name: '', cards: FishCard.values),
-    );
-    cardoteka.setMockInitialValues({});
-    await Cardoteka.init();
-  });
-
-  tearDown(() {
-    cardoteka.deInit();
-  });
-
-  // attachedCallback;
-  // setUpAll(() {});
-
-  group('$WatcherImpl.attach -> What does attach return when SP is empty?', () {
-    const reason = 'Should always return to default';
-    test('If non-nullable card', () {
-      const card = FishCard.perch;
-      final value = cardoteka.attach(card, (_) {}, detacher: null);
-
-      expect(value, card.defaultValue, reason: reason);
-    });
-
-    test('If nullable card with non-null default value', () {
-      const card = FishCard.perchGhost;
-      final value = cardoteka.attach(card, (_) {}, detacher: null);
-
-      expect(value, card.defaultValue, reason: reason);
-    });
-
-    test('If nullable card with null default value', () {
-      const card = FishCard.perchGhostNull;
-      final value = cardoteka.attach(card, (_) {}, detacher: null);
-
-      expect(value, card.defaultValue, reason: reason);
-    });
-  });
-
-  group('$WatcherImpl.attach -> Correct operation of callback', () {
-    const reason =
-        'The callback should return a new value as soon as it changes'
-        ' in the store';
-
-    test('Checking operations that call a callback', () async {
-      const card = FishCard.perch;
-      final doubledValue = card.defaultValue * 2;
-      final tripledValue = card.defaultValue * 3;
-      int counter = 0;
-      final actionForResultInCallback = <AsyncCallback, int>{
-        () async => cardoteka.set<int>(card, doubledValue): doubledValue,
-        () async => cardoteka.remove(card): card.defaultValue,
-        () async => cardoteka.setOrNull<int>(card, tripledValue): tripledValue,
-        () async => cardoteka.removeAll(): card.defaultValue,
-      }.entries.toList();
-      cardoteka.attach<int>(
-        card,
-        (cbValue) async {
-          expect(
-            cbValue,
-            actionForResultInCallback[counter++].value,
-            reason: reason,
-          );
-        },
-        detacher: null,
-      );
-
-      await Future.wait(actionForResultInCallback.map((e) => e.key()));
-
-      expect(
-        actionForResultInCallback,
-        hasLength(counter),
-        reason: 'All operations must trigger the callback',
-      );
-    });
+Future<void> testWith(
+  Object description,
+  dynamic Function() body, {
+  dynamic Function()? setUp,
+  dynamic Function()? tearDown,
+}) async {
+  test(description, () async {
+    await setUp?.call();
+    await body.call();
+    await tearDown?.call();
   });
 }
+
+Future<void> main() async {
+  for (final config in cardsCollections) {
+    late CardotekaTest cardoteka;
+    Future<void> setUpAction() async {
+      print('setUp');
+      cardoteka = CardotekaTest(config: config);
+      cardoteka.setMockInitialValues({});
+      await Cardoteka.init();
+    }
+
+    Future<void> tearDownAction() async {
+      print('tearDown');
+      cardoteka.deInit();
+    }
+
+    await testWith(
+      '$WatcherImpl.attach -> Checking operations that call a callback',
+      setUp: setUpAction,
+      tearDown: tearDownAction,
+      () async {
+        for (final card in cardoteka.cards) {
+          if (!isPrimitiveValue(card, elseNull: true)) continue;
+
+          final Object? defaultValue = card.defaultValue;
+          final Object? newValue = getTestValueBasedOnDefaultValue(card);
+          final actionForResultInCallback =
+              <(AsyncCallback action, Object? expectedValue)>[
+            if (newValue != null)
+              (() async => cardoteka.set(card, newValue), newValue),
+            (() async => cardoteka.remove(card), defaultValue),
+            (() async => cardoteka.setOrNull(card, newValue), newValue),
+            (() async => cardoteka.removeAll(), defaultValue),
+          ];
+          int counter = 0;
+
+          late final void Function() detacher;
+          cardoteka.attach(
+            card,
+            (cbValue) => expect(
+              cbValue,
+              actionForResultInCallback[counter++].$2,
+              reason: getReason(
+                'The callback should return a new value as soon as it changes in the store'
+                '$actionForResultInCallback, when count is $counter',
+                card,
+              ),
+            ),
+            detacher: (_) => detacher = _,
+          );
+          cardoteka.printAllWatchers();
+          await Future.wait(actionForResultInCallback.map((e) => e.$1.call()));
+
+          expect(
+            actionForResultInCallback,
+            hasLength(counter),
+            reason: getReason(
+              'All operations must trigger the callback',
+              card,
+            ),
+          );
+          detacher.call();
+        }
+      },
+    );
+
+    await testWith(
+      '$WatcherImpl.attach -> What does attach return when storage is empty?',
+      setUp: setUpAction,
+      tearDown: tearDownAction,
+      () async {
+        for (final card in cardoteka.cards) {
+          final value = cardoteka.attach(card, (_) {}, detacher: null);
+          expect(
+            value,
+            card.defaultValue,
+            reason: getReason(
+              'Should always return to default value!',
+              card,
+            ),
+          );
+        }
+      },
+    );
+
+    await testWith(
+      '$WatcherImpl.attach -> What does attach return when storage contains value?',
+      setUp: setUpAction,
+      tearDown: tearDownAction,
+      () async {
+        for (final card in cardoteka.cards) {
+          if (!isPrimitiveValue(card, elseNull: false)) continue;
+
+          final testedValue = getTestValueBasedOnDefaultValue(card);
+          final isSuccess = await cardoteka.setOrNull(
+            card,
+            getTestValueBasedOnDefaultValue(card),
+          );
+          expect(
+            isSuccess,
+            isTrue,
+            reason: getReason(
+              'The value must be saved in the storage',
+              card,
+            ),
+          );
+
+          final value = cardoteka.attach(card, (_) {}, detacher: null);
+          expect(
+            value,
+            testedValue,
+            reason: getReason(
+              'Should return a value that is contained in the storage!',
+              card,
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+// todo:
+//   test('Registering several callbacks', () async {});
+//   test('Detacher Functional Check', () async {});
+//   test('More than one listener per card', () async {});
+//   test('FireImmediately', () async {});
