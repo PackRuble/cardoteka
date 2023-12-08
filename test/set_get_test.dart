bf55e92c916088a995704c7cbed409b4a764602d
Ruble <pack.ruble@gmail.com>
2023-12-08 11:13:58 +0300

    add: set|get test based on ForestCard

--- test/set_get_test.dart
+++ test/set_get_test.dart
@@ -1,67 +1,88 @@
-import 'dart:ui';
+import 'dart:ui' show Color;
 
 import 'package:cardoteka/cardoteka.dart';
+import 'package:cardoteka/src/core.dart' show CardotekaUtilsForTest;
 import 'package:flutter_test/flutter_test.dart';
 
-import 'source/forest_key_store.dart';
+import 'source/cards.dart';
 
-class ForestDb extends Cardoteka {
-  const ForestDb({required super.cards, required super.config});
+class CardotekaImplTest extends Cardoteka with CardotekaUtilsForTest {
+  CardotekaImplTest({required super.config});
 }
 
-void main() {
-  const db = ForestDb(
-    cards: ForestCard.values,
-    config: forestCardConfig,
+Future<void> main() async {
+  const newValuesByCard = <ForestCard<Object?>, Object>{
+    ForestCard.keepAcacia: false,
+    ForestCard.keepAcaciaWithNull: false,
+    ForestCard.keepAcaciaWCK: false,
+    ForestCard.ageBaobab: 50,
+    ForestCard.ageBaobabWCK: 79,
+    ForestCard.diameterTrunkBirch: 50.0,
+    ForestCard.diameterTrunkBirchWCK: 31.999,
+    ForestCard.habitatOak: 'Other regions',
+    ForestCard.habitatOakWCK: 'Regions',
+    ForestCard.namesLinden: [],
+    ForestCard.namesLindenWCK: ['Linden1', 'Linden2'],
+    ForestCard.currentColorSpruce: Color(0xFFAB6F00),
+    ForestCard.currentColorSpruceWCK: Color(0x8CFF008C),
+    ForestCard.lifetimeCedar: Duration(days: 600 * 365),
+  };
+
+  test(
+    'The number of test value cards is the same as the number of all cards',
+    () => expect(newValuesByCard, hasLength(ForestCard.values.length)),
   );
 
-  db.setMockInitialValues({});
-
-  test('Simple getting/setting by Enum.key', () async {
-    // we may to call ForestDb() - it's the same thing
-
-    await db.init();
-
-    // Simple reading by key
-    for (final key in ForestCard.values) {
-      db.getOrNull(key);
-    }
-
-    final newSetup = <ForestCard<Object>, Object>{
-      ForestCard.keepAcacia: false,
-      ForestCard.keepAcaciaWCK: false,
-      ForestCard.ageBaobab: 50,
-      ForestCard.ageBaobabWCK: 79,
-      ForestCard.diameterTrunkBirch: 50.0,
-      ForestCard.diameterTrunkBirchWCK: 31.999,
-      ForestCard.habitatOak: 'Other regions',
-      ForestCard.habitatOakWCK: 'Regions',
-      ForestCard.namesLinden: [],
-      ForestCard.namesLindenWCK: ['Linden1', 'Linden2'],
-      ForestCard.currentColorSpruce: const Color(0xFFAB6F00),
-      ForestCard.currentColorSpruceWCK: const Color(0x8CFF008C),
-    };
-
-    // Simple setting by key
-    for (final entry in newSetup.entries) {
-      await db.set(entry.key, entry.value);
-    }
-
-    // We get the values to compare with [newSetup]:
-    for (final entry in newSetup.entries) {
-      expect(
-        db.get(entry.key),
-        entry.value,
-      );
-    }
-
-    expect(db.get(ForestCard.keepAcacia), isNotNull);
-    expect(db.get(ForestCard.keepAcacia), isA<bool>());
-    expect(db.getOrNull(ForestCard.keepAcacia), isA<bool?>());
-
-    expect(db.getOrNull(ForestCard.keepAcaciaWithNull), isA<bool?>());
-
-    await db.set(ForestCard.keepAcaciaWithNull, false);
-    expect(db.getOrNull(ForestCard.keepAcaciaWithNull), isNotNull);
+  late CardotekaImplTest cardoteka;
+  setUp(() async {
+    cardoteka = CardotekaImplTest(
+      config: allCardConfigs.firstWhere((el) => el.name == '$ForestCard'),
+    );
+    cardoteka.setMockInitialValues({});
+    await Cardoteka.init();
   });
+
+  tearDown(() => cardoteka.deInit());
+
+  test(
+    "$Cardoteka.getOrNull-> Returns null if haven't saved values before",
+    () {
+      for (final card in ForestCard.values) {
+        final result = cardoteka.getOrNull(card);
+        expect(result, isNull);
+      }
+    },
+  );
+
+  test(
+    "$Cardoteka.get-> Returns default value if haven't saved values before",
+    () {
+      for (final card in ForestCard.values) {
+        // because [get] only accepts non-null cards.
+        if (card == ForestCard.keepAcaciaWithNull) continue;
+
+        final result = cardoteka.get(card as Card<Object>);
+        expect(result, card.defaultValue);
+      }
+    },
+  );
+
+  test(
+    "$Cardoteka.set->get&getOrNull-> Returns the set value",
+    () async {
+      for (final card in ForestCard.values) {
+        final savedValue = newValuesByCard[card]!;
+        final isSuccessSet = await cardoteka.set(card, savedValue);
+        expect(isSuccessSet, isTrue);
+
+        final resultOrNull = cardoteka.getOrNull(card);
+        expect(resultOrNull, savedValue);
+
+        // because [get] only accepts non-null cards.
+        if (card == ForestCard.keepAcaciaWithNull) continue;
+        final result = cardoteka.get(card as Card<Object>);
+        expect(result, savedValue);
+      }
+    },
+  );
 }
