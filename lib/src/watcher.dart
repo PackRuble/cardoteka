dab857e86c88d8282d130072371eecb319e948a6
Ruble <pack.ruble@gmail.com>
2023-12-16 15:21:39 +0300

    upd: the `protected` annotation was changed to `internal` because it incorrectly reflected the intent. Now that gives the right clues.

--- lib/src/watcher.dart
+++ lib/src/watcher.dart
@@ -10,6 +10,6 @@ import 'card.dart';
 abstract class Watcher {
   /// Called whenever a new value is provided for the storage. To implement
   /// this method, please refer to the documentation [Cardoteka.watcher].
-  @protected
+  @internal
   void notify<V extends Object?>(Card<V> card, V value);
 }
