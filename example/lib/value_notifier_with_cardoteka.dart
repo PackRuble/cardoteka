ec1e52d87006fa296235d856924fdb2939c696b0
Ruble <pack.ruble@gmail.com>
2025-01-17 15:30:41 +0300

    upd: example: use `DetacherChangeNotifier` for `ValueNotifier` and use shared data

--- example/lib/value_notifier_with_cardoteka.dart
+++ example/lib/value_notifier_with_cardoteka.dart
@@ -1,43 +1,61 @@
 import 'package:cardoteka/cardoteka.dart';
-import 'package:flutter/material.dart' hide Card;
+import 'package:flutter/material.dart';
 
-/// Example of using [Cardoteka] and [WatcherImpl] mixin with [ValueNotifier].
+import 'app_cardoteka.dart';
 
-class CurrentTaskNotifier extends ValueNotifier {
-  CurrentTaskNotifier(super.task);
+// I created an instance of Cardoteka and cards earlier, and here I'm just
+// showing you their types and uses
+final AppCardoteka cardoteka = appCardoteka;
+const AppSettings<bool> card = AppSettings.isPremium; // with defaultValue=false
 
-  VoidCallback? _onDetach;
+/// An example of using [Cardoteka] with the [WatcherImpl] and
+/// [DetacherChangeNotifier] mixins for the [ValueNotifier] state class.
+class PremiumNotifier extends ValueNotifier<bool> with DetacherChangeNotifier {
+  PremiumNotifier(super.isPremium);
 
-  void onDispose(void Function() cb) => _onDetach = cb;
+  Future<void> checkPremium() async {
+    final bool result = await Future.delayed(
+      // simulate server request delay
+      const Duration(milliseconds: 100),
+      () => true,
+    );
 
-  @override
-  void dispose() {
-    _onDetach?.call();
-    super.dispose();
+    await cardoteka.set(card, result);
   }
 }
 
-final class CardotekaImpl = Cardoteka with WatcherImpl;
-
 Future<void> main() async {
   await Cardoteka.init();
-  // ignore_for_file: definitely_unassigned_late_local_variable
-  // to☝️do: create an instance of cardoteka and pass configuration with cards
-  late CardotekaImpl cardoteka;
-  late Card<String> card; // with default value = 'no business...'
 
-  final notifier = CurrentTaskNotifier('');
+  // We get a previously saved value from storage.
+  // If isn't present, `card.defaultValue` will be returned.
+  final isPremium = cardoteka.get(card);
+  final premiumNR = PremiumNotifier(isPremium);
+  print('1️⃣State is premium?: value=${premiumNR.value}');
+
   cardoteka.attach(
     card,
-    (value) {
-      notifier.value = value;
-      print('New case: $value');
-    },
-    detacher: notifier.onDispose, // attention to this line
+    (value) => premiumNR.value = value,
+    detacher: premiumNR.onDetach, // a line that allows you to fix memory leaks
   );
 
-  await cardoteka.set(card, 'new case available!');
-  // 1. console-> New case: no business...
-  // 2. a value was saved to storage
-  // 3. console-> New case: new case available!
+  await premiumNR.checkPremium();
+  print('2️⃣State is premium?: value=${premiumNR.value}');
+
+  await cardoteka.set(card, false);
+  print('3️⃣State is premium?: value=${premiumNR.value}');
+
+  premiumNR.dispose();
+
+  // What happened?
+  // 1. Get current value from storage by card
+  // 2. console-> 1️⃣State is premium?: value=false
+  // 3. Attach a watcher to this card, which will notify the notifier about new values
+  // 4. Check premium on the server by calling `PremiumNotifier.checkPremium` method
+  // 5. console-> 2️⃣State is premium?: value=true
+  // 6. We save the new value to cardoteka, and after triggering watcher..:
+  // 7. console-> 3️⃣State is premium?: value=false
+  //
+  // That is, roughly speaking, we can have very many notifiers with wiretapping attached
+  // that will automatically update the state after the values in the storage change.
 }
