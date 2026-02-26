571ff977ff2a4c1a8d6829fb483df7fcbe6a1f2e
Ruble <pack.ruble@gmail.com>
2026-02-26 20:22:28 +0300

    add: pure dart project using only the cardoteka package. related with #51

---
+++ example_new/dart_project/lib/dart_project.dart
@@ -0,0 +1,43 @@
+import 'package:cardoteka/cardoteka.dart';
+
+enum SettingsCard<T> implements Card<T> {
+  isPremium(DataType.bool, false),
+  premiumPurchaseDay<DateTime?>(DataType.string, null),
+  ;
+
+  const SettingsCard(this.type, this.defaultValue);
+
+  @override
+  final DataType type;
+
+  @override
+  final T defaultValue;
+
+  @override
+  String get key => name;
+
+  static const converters = <Card, Converter>{
+    premiumPurchaseDay: Converters.dateTimeAsString,
+  };
+}
+
+void main() async {
+  final cardoteka = Cardoteka(
+    config: const CardotekaConfig(
+      cards: SettingsCard.values,
+      converters: SettingsCard.converters,
+    ),
+    storage: MemoryStorage(),
+  );
+
+  final isPremium = cardoteka.getOrDefault(SettingsCard.isPremium);
+  if (!isPremium) {
+    // Well, we'll buy it today.
+    // ignore_for_file: avoid_print
+    await Future.delayed(const .new(seconds: 5), () => print('Bought!'));
+    await cardoteka.set(SettingsCard.premiumPurchaseDay, DateTime.timestamp());
+
+    final date = cardoteka.get(SettingsCard.premiumPurchaseDay);
+    print('Congratulations, the premium was purchased on $date!');
+  }
+}
